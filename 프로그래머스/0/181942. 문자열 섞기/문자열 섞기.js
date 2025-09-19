7ff0f72ef0a35a1d1ac148ccb3011a9c2d21251b
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-09-19 20:10:41 +0900

    [level 0] Title: 문자열 섞기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -1,9 +1,7 @@
 function solution(str1, str2) {
     var answer = '';
-    let str = "";
     for(let i = 0; i < str1.length; i++) {
-        str = str1[i] + str2[i];
-        answer += str;
+        answer += str1[i] + str2[i];
     }
     
     return answer;
