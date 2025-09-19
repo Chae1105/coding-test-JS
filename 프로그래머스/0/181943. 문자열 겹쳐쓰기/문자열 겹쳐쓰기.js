5243f27f073f8395f91ebad5eca995f3953c712c
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-09-19 19:55:32 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -1,5 +1,5 @@
 function solution(my_string, overwrite_string, s) {
-    var answer = my_string.slice(0, s) + overwrite_string + my_string.slice(overwrite_string.length+s,);
+    var answer = my_string.slice(0, s) + overwrite_string + my_string.slice(overwrite_string.length+s);
     
     return answer;
 }
