dc0eda2c38bf885977bff64ec0cb4d0033960710
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-09-22 14:22:22 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,7 +1,5 @@
 function solution(n) {
     let arr = [];
-    for(let i = 1; i <= n; i++) {
-        if(i%2 === 1) arr.push(i);
-    }
+    for(let i = 1; i <= n; i+=2) arr.push(i);
     return arr;
 }
