220258225c2a7ee59914360a2d70b9e193786c2e
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-09-22 14:59:33 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.js
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.js
@@ -1,4 +1,7 @@
 function solution(my_string) {
+    // return [...my_string].reverse().join('');
+    
+    // reverse 함수 안 쓴다면
     let reverse_string = "";
     
     for (let i = my_string.length - 1; i >= 0; i--) {
