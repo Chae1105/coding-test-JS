767d6104aab6335d4b17c57e13778168841be520
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-09-23 21:36:37 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,3 +1,16 @@
 function solution(num_list) {
-    return [num_list.filter((num) => num%2 === 0).length, num_list.filter((num) => num%2 === 1).length]
+    /*
+    return [
+        num_list.filter((num) => num%2 === 0).length, 
+        num_list.filter((num) => num%2 === 1).length
+    ]
+    */
+    
+    let result = [0, 0];
+    
+    for(let num of num_list) {
+        result[num%2] += 1;
+    }
+    
+    return result;
 }
