4e13dce57123bc8add01bc5919f483e90d0eade1
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-09-23 21:41:09 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -1,7 +1,11 @@
 function solution (my_string, n) {
+    /*
     let result = "";
     for (let a of my_string) {
         result += a.repeat(n);
     }
     return result;
+    */
+    
+    return [...my_string].map(a => a.repeat(n)).join("");
 }
