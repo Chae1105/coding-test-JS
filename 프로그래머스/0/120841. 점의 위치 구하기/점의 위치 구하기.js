8da89a6a33274a7273e025bd404c8a1128ae7222
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-09-29 23:16:23 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -2,4 +2,9 @@ function solution(dot) {
     const [x, y] = dot;
     
     return x > 0 && y > 0 ? 1 : x < 0 && y > 0 ? 2 : x < 0 && y < 0 ? 3 : 4;
+    
+    /*
+    const check = x * y > 0;
+    return x > 0 ? (check ? 1 : 4) : (check ? 3 : 2);
+    */
 }
