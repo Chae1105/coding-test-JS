59fedaecb9db6ba6037218ee3cf28afbe5a1e265
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-10-03 16:39:54 +0900

    [level 1] Title: 폰켓몬, Time: 0.58 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,5 +1,5 @@
 function solution(nums) {
-    /*
+    /* 배열의 길이와, 배열 내 요소들에 종류의 수를 비교해서 더 적은 값을 리턴
     const obj = {};
     
     for(let num of nums) {
@@ -7,11 +7,13 @@ function solution(nums) {
     }
     
     let keys = Object.keys(obj).length;
+    let num = nums.length/2
     
-    return Math.min(keys, nums.length/2);
+    return Math.min(keys, num);
     */
     
-    const numSet = new Set(nums).size;
+    // 이게 시간을 훨씬 더 적게 소요
+    const num = new Set(nums).size; // set: 중복값 제거
     
-    return Math.min(numSet, nums.length/2)
+    return Math.min(num, nums.length/2); 
 }
