b9b90f6dad617cc66159d4d2c9a19095ae792fa6
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-10-03 16:51:18 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -0,0 +1,8 @@
+function solution(numbers) {
+    numbers.sort((a, b) => a - b);
+    
+    const first = numbers[0] * numbers[1];
+    const last = numbers.at(-1) * numbers.at(-2);
+    
+    return first >= last ? first : last;
+}
