6dac6e20e3f4d2aeda95bb669a2a624a07f2e348
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-10-04 20:12:40 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,4 +1,9 @@
 function solution(array) {
+    /*
     const maxN = [...array].sort((a, b) => a - b).at(-1);
+    return [maxN, array.indexOf(maxN)];
+    */
+    
+    const maxN = Math.max(...array);
     return [maxN, array.indexOf(maxN)];
 }
