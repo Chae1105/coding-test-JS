999c7e38157833364a49dcd325b7f477e200165d
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-10-07 22:47:55 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,4 +1,5 @@
 function solution(numbers) {
+    /*
     const num = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     
     let str = "";
@@ -12,4 +13,16 @@ function solution(numbers) {
     }
     
     return Number(answer);
+    */
+    
+    const obj = {
+        zero: 0, one: 1, two: 2, three: 3, four: 4,
+        five: 5, six: 6, seven: 7, eight: 8, nine: 9
+    };
+
+    const num = numbers.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, (v => {
+        return obj[v];
+    }));
+
+    return Number(num);
 }
