ab861cb9819f7b5571362bdce8d63f3f9484e784
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-10-09 16:33:48 +0900

    [level 0] Title: k의 개수, Time: 21.56 ms, Memory: 44.6 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/0/120887. k의 개수/k의 개수.js
@@ -1,5 +1,5 @@
 function solution(i, j, k) {
-    // 정수들을 문자열로 변환한 뒤, k를 제외한 문자들을 제거한 후 남은 문자열의 길이로 k의 개수 찾기
+    /* 정수들을 문자열로 변환한 뒤, k를 제외한 문자들을 제거한 후 남은 문자열의 길이로 k의 개수 찾기
     let answer = 0;
     let regexp = new RegExp(`[^${k}]`, 'g');
     for(let n = i; n <= j; n++) {
@@ -7,4 +7,14 @@ function solution(i, j, k) {
     }
     
     return answer;
+    */
+    
+    // k가 n개가 있으면, 배열의 길이는 n+1개가 됨
+    let answer = '';
+    
+    for(let x = i; x <= j; x++) {
+        answer += x;
+    }
+    
+    return answer.split(k).length - 1;
 }
