31f29963584d08d08234575048ef0110913440b3
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-10-09 17:28:31 +0900

    [level 0] Title: 이진수 더하기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.js
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.js
@@ -1,3 +1,25 @@
 function solution(bin1, bin2) {
-    return (parseInt(bin1, 2) + parseInt(bin2, 2)).toString(2);
+    // return (parseInt(bin1, 2) + parseInt(bin2, 2)).toString(2);
+    
+    const n1 = bin1.split('').reverse().reduce((sum, n, idx) => sum += n * Math.pow(2, idx), 0);
+    const n2 = bin2.split('').reverse().reduce((sum, n, idx) => sum += n * Math.pow(2, idx), 0);
+    
+    let sum = n1 + n2;
+    
+    let answer = '';
+
+    if(sum === 0) {
+        return "0";
+    }
+    
+    for(let i = 11; i >= 0; i--) {
+        const n = Math.pow(2, i)
+        
+        if(sum >= n) {
+            answer += "1";
+            sum -= n;
+        } else if(answer.length !== 0) answer += "0";
+    }
+    
+    return answer;
 }
