ccd48a605161404eb767ee71fd3f5ef088e34ee9
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-10-10 19:15:09 +0900

    [level 0] Title: 로그인 성공?, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,4 +1,5 @@
 function solution(id_pw, db) {
+    /*
     for(let user of db) {
         if(user[0] === id_pw[0]) {
             if(user[1] === id_pw[1]) return "login";
@@ -6,4 +7,11 @@ function solution(id_pw, db) {
         } 
     }
     return "fail";
+    */
+    
+    const [id, pw] = id_pw;
+    
+    const map = new Map(db);
+    
+    return map.has(id) ? (map.get(id) === pw ? "login" : "wrong pw") : "fail";
 }
