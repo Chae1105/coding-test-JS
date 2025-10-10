522edad82cc41a1f2d16703e661f0242ee8ceac0
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-10-10 19:28:59 +0900

    [level 0] Title: 등수 매기기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
@@ -0,0 +1,11 @@
+function solution(score) {
+    const avg = score.map(x => (x[0] + x[1])/2);
+    const avgSort = [...avg].sort((a, b) => b - a);
+    
+    let answer = [];
+    for(let n of avg) {
+        answer.push(avgSort.indexOf(n) + 1);
+    }
+    
+    return answer;
+}
