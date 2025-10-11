b685fdda000d28764b3eaa3be3b7a51f80b5591a
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-10-11 17:24:53 +0900

    [level 0] Title: 다항식 더하기, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -0,0 +1,25 @@
+function solution(polynomial) {
+    let arr = polynomial.split(' + ');
+    
+    let x = 0;
+    let num = 0;
+    let answer = "";
+    
+    for(let el of arr) {
+        if(el.includes('x')) {
+            x += Number(el.replace(/x/, '')) || 1;
+        } else {
+            num += Number(el);
+        }
+    }
+    
+    if(x !== 0 && num !== 0) {
+        if(x === 1) return `x + ${num}`;
+        else return `${x}x + ${num}`;
+    } else if(x === 0) {
+        return `${num}`;
+    } else {
+        if(x === 1) return `x`;
+        else return `${x}x`;
+    }
+}
