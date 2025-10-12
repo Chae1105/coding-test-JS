9d062a288b975d75f2b59d3149cfd0c3e940098d
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-10-12 17:09:44 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,4 +1,5 @@
 function solution(my_string) {
+    /*
     let str =  my_string.replace(/[a-zA-Z]/g, " ");
     
     let num = "";
@@ -16,5 +17,16 @@ function solution(my_string) {
     answer.push(+num); // for문에서 마지막에 위치한 숫자는 push를 못 해줌
     
     return answer.reduce((sum, n) => sum += n, 0);
+    */
     
+    // match: 배열 반환, 매칭되는게 없으면 null 반환
+    // \d+ : 연속된 숫자들
+    /*
+    let answer = my_string.match(/\d+/g); 
+    
+    return answer ? answer.reduce((sum, n) => sum += Number(n), 0) : 0;
+    */
+    
+    return my_string.split(/\D+/).reduce((acc, cur) => acc + Number(cur), 0);
+
 }
