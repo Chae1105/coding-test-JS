7fbbba508ebcfdcc4bb6dade9560f58e15a34aa2
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-10-13 21:47:14 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,4 +1,5 @@
 function solution(a, b) {
+    /*
     for(let i = 2; i <= a; i++) {
         if(a%i === 0 && b%i === 0) {
             a /= i;
@@ -8,16 +9,6 @@ function solution(a, b) {
     
     let n = 2;
     let answer = [];
-    
-    /*
-    for(n; n <= b; n++) {
-        if(b % n === 0) {
-            b /= n;
-            answer.push(n);
-            n = 2;
-        }
-    }
-    */
     
     while (b > 1) {
         if(b%n === 0) {
@@ -30,4 +21,16 @@ function solution(a, b) {
 
     // every : 배열의 모든 요소가 조건을 만족할 때에만 true 반환
     return answer.every(x => x === 2 || x === 5) ? 1 : 2;
+    */
+    
+    let n = 1;
+    for (let i = 1; i <= Math.min(a,b); i++) {
+        if (a%i===0 && b%i===0) n = i;
+    } // a, b의 최대공약수 구하기
+
+    b/=n;
+    while (b%2===0) b/=2;
+    while (b%5===0) b/=5;
+
+    return b === 1 ? 1 : 2; // 2와 5로만 나눈 후의 값이 1인지 확인
 }
