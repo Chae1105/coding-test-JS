061c796859e401785c7db439df70c7fa923cf817
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-10-14 19:19:34 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -1,4 +1,5 @@
 function solution(n) {
+    /*
     let i = 1;
     
     while(Math.pow(i, 2) <= n) {
@@ -7,4 +8,8 @@ function solution(n) {
     }
     
     return 2;
+    */
+    
+    // isInteger : 정수인지 판별하는 함수
+    return Number.isInteger(Math.sqrt(n)) ? 1 : 2;
 }
