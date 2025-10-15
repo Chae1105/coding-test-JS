2d8e75656a498aefc9c7fa6e7e14f806a8826bc9
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-10-15 14:52:59 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
@@ -1,3 +1,7 @@
 function solution(my_string) {
+    /*
     return my_string.split('').map(x => x.toLowerCase()).sort().join('');
+    */
+    
+    return my_string.toLowerCase().split('').sort().join('');
 }
