6d3b0f45880079bdcb620c5a98740753eab928f5
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-10-15 15:04:38 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -0,0 +1,10 @@
+function solution(my_str, n) {
+    let answer = [];
+    
+    while(my_str.length !== 0) {
+        answer.push(my_str.slice(0, n));
+        my_str = my_str.slice(n, my_str.length);
+    }
+    
+    return answer;
+}
