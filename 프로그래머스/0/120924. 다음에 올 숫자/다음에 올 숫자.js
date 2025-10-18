98f498852f19ddf7fe20d1856fe388d8de198d2b
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-10-18 21:51:47 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,5 +1,7 @@
 function solution(common) {
     const [x, y, z] = common;
     
-    return y - x === z - y ? common.at(-1) + (y - x) : common.at(-1) * (y / x);
+    //return y - x === z - y ? common.at(-1) + (y - x) : common.at(-1) * (y / x);
+    
+    return y - x === z - y ? common.pop() + (y - x) : common.pop() * (y / x);
 }
