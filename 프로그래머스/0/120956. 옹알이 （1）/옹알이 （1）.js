a99f613ffd26cf961e7fedba6bef2744313f7c5f
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-10-18 22:03:48 +0900

    [level 0] Title: 옹알이 (1), Time: 0.37 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -0,0 +1,18 @@
+function solution(babbling) {
+    let can = ["aya", "ye", "woo", "ma"];
+    let answer = 0;
+    
+    for(let s of babbling) {
+        let str = "";
+        
+        for(let i = 0; i < s.length; i++) {
+            str += s[i];
+            if(can.includes(str)) {
+                str = "";
+            }
+        }
+        if(str.length === 0) answer++;
+    }
+    
+    return answer;
+}
