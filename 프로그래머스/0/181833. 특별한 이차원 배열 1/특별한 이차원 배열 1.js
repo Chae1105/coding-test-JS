01b366c35daa7883cd4ef7759c3d47a27c2b0b49
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-10-20 17:00:26 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.28 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -1,12 +1,8 @@
 function solution(n) {
     let answer = Array.from({length : n}, () => Array(n).fill(0));
-    let i = 0;
     
-    for(let arr of answer) {
-        console.log(arr);
-        arr[i] = 1;
-        i++;
-        console.log(arr);
+    for(let i = 0; i < n; i++) {
+        answer[i][i] = 1;
     }
     
     return answer;
