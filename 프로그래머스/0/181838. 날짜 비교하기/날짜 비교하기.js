973cccfc62850703cfae451ba7fa1c7eeba2cd95
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-10-21 20:42:27 +0900

    [level 0] Title: 날짜 비교하기, Time: 73.00 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,4 +1,5 @@
 function solution(date1, date2) {
+    /*
     if(date1[0] === date2[0]) {
         if(date1[1] === date2[1]) {
             return date1[2] < date2[2] ? 1 : 0;
@@ -8,4 +9,7 @@ function solution(date1, date2) {
     } else if(date1[0] < date2[0]) {
         return 1;
     } else return 0;
+    */
+    
+    return new Date(date1) < new Date(date2) ? 1 : 0;
 }
