42dfe2f1d92be196d83cd571656eb4ceded96691
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-10-24 22:36:30 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.js
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.js
@@ -1,3 +1,5 @@
 function solution(num_list) {
-    return num_list.sort((a, b) => a - b).filter((_, idx) => idx > 4);
+    //return num_list.sort((a, b) => a - b).filter((_, idx) => idx > 4);
+    
+    return num_list.sort((a, b) => a - b).slice(5);
 }
