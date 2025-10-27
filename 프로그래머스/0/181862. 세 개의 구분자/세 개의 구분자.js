2e80555a3182a29b4de1640dec15a63461e323e8
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-10-27 17:52:49 +0900

    [level 0] Title: 세 개의 구분자, Time: 14.38 ms, Memory: 61.7 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,4 +1,8 @@
 function solution(myStr) {
+    /*
     const answer = myStr.split(/[abc]/).filter(str => str !== "");
     return answer.length !== 0 ? answer : ["EMPTY"];
+    */
+    
+    return myStr.match(/[^a-c]+/g) || ["EMPTY"];
 }
