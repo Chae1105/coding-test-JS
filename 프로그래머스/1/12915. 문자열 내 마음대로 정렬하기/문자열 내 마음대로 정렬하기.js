ce16c1d505ec58d0112dcfb7e64f3fe42a0e2733
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-10-29 21:34:57 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 31.26 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,7 +1,11 @@
 function solution(strings, n) {
     // sort((a, b) => 콜백함수) : 반환값이 음수면 a가 앞으로, 양수면 b가 앞으로, 0이면 순서 유지
+    /*
     return strings.sort((a,b) => {
         if(a[n] !== b[n]) return a[n] > b[n] ? 1 : -1;
         else return a > b ? 1 : -1;
     });
+    */
+    
+    return strings.sort((a,b) => a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n]));
 }
