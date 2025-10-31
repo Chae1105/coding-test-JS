302a343d3280a943cc11332883961e7a2f1377c9
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-10-31 21:26:42 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
@@ -1,5 +1,6 @@
 function solution(my_string, alp) {
     const reg = new RegExp(alp, "g");
     
-    return my_string.replace(reg, alp.toUpperCase());
+    //return my_string.replace(reg, alp.toUpperCase());
+    return my_string.replaceAll(alp, alp.toUpperCase());
 }
