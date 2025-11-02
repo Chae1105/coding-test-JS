f423a37acf0380ef3853ab15168b79c13630375c
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-11-02 18:25:58 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,4 +1,9 @@
 function solution(s) {
+    /*
     const answer = s.split(" ").map(n => +n).sort((a, b) => a - b);
-    return `${answer[0]} ${answer[answer.length-1]}`;
+    return `${answer[0]} ${answer.at(-1)}`;
+    */
+    
+    const answer = s.split(" ");
+    return `${Math.min(...answer)} ${Math.max(...answer)}`;
 }
