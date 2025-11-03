0354842e845172d99076deec37b3ae4247db99f9
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-11-03 21:26:41 +0900

    [level 2] Title: 올바른 괄호, Time: 29.75 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,4 +1,5 @@
 function solution(s) {
+    /*
     if(s[0] === ")" || s[s.length - 1] === "(") return false;
 
     const stack = [];
@@ -13,4 +14,15 @@ function solution(s) {
     }
     
     return stack.length === 0 ? true : false;
+    */
+    
+    let answer = 0;
+    
+    for(let c of s) {
+        answer += c === "(" ? 1 : -1;
+        
+        if(answer < 0) return false;
+    }
+    
+    return answer === 0 ? true : false;
 }
