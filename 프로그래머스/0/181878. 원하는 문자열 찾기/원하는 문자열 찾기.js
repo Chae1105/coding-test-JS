d5232c02391f968f0af729ddb4e79bd24f5cac10
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-11-03 21:36:47 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.16 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -1,3 +1,4 @@
 function solution(myString, pat) {
-    return myString.toLowerCase().indexOf(pat.toLowerCase()) < 0 ? 0 : 1;
+    //return myString.toLowerCase().indexOf(pat.toLowerCase()) < 0 ? 0 : 1;
+    return myString.toLowerCase().includes(pat.toLowerCase()) ? 1 : 0;
 }
