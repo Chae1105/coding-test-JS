e43599942876cc5cd546035bff1c5c49372949f0
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-11-04 22:07:23 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 26.27 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -1,7 +1,11 @@
 function solution(arr) {
+    /*
     return arr.map(n => {
         if(n < 50 && n % 2 === 1) return n * 2;
         else if(n >= 50 && n % 2 === 0) return n / 2;
         else return n;
     })
+    */
+    
+    return arr.map(n => n<50 && n%2 === 1 ? n*2 : n>=50 && n%2 === 0 ? n/2 : n);
 }
