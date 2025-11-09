f6471874fb5bd079fe4d9a1cf63b57d68b2a4637
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-11-09 16:37:15 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,6 +1,15 @@
 function solution(num_list) {
+    /*
     const sum1 = num_list.reduce((sum, n, i) => i % 2 === 0 ? sum += n : sum, 0);
     const sum2 = num_list.reduce((sum, n, i) => i % 2 === 1 ? sum += n : sum, 0);
     
     return Math.max(sum1, sum2);
+    */
+    
+    let even = 0;
+    let odd = 0;
+    
+    num_list.map((num, idx) => idx % 2 === 0 ? even += num : odd += num);
+    
+    return Math.max(even, odd);
 }
