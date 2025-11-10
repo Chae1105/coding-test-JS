fca99fbf371b557afb3cfe02f570ca1c98095a74
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-11-10 23:58:22 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.04 ms, Memory: 32.6 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.js
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.js
@@ -1,3 +1,8 @@
 function solution(num_list, n) {
-    return [...num_list.slice(n), ...num_list.slice(0, n)];
+    //return [...num_list.slice(n), ...num_list.slice(0, n)];
+    
+    const answer = num_list.splice(n);
+    answer.push(...num_list);
+    
+    return answer;
 }
