34c29810c73888f4911bfbc0081912fca144a93f
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-11-28 22:24:25 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,7 +1,11 @@
 function solution(num_list) {
+    /*
     for(let idx in num_list) {
         if(num_list[idx] < 0) return +idx
     }
     
     return -1;
+    */
+    
+    return num_list.findIndex(n => n < 0);
 }
