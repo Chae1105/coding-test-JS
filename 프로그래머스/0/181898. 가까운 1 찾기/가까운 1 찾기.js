253a735d5314d6a9aa909591f38ab97e823b729e
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-11-28 22:30:36 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,7 +1,11 @@
 function solution(arr, idx) {
+    /*
     for(let i in arr) {
         if(+i >= idx && arr[i] === 1) return +i;
     }
     
     return -1;
+    */
+    
+    return arr.indexOf(1, idx);
 }
