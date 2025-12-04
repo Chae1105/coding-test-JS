230e4f61deb0491378c6c4dfe97d1eb72393c7cc
chaelin <83539023+Chae1105@users.noreply.github.com>
2025-12-04 16:03:30 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,8 +1,14 @@
 function solution(num_list) {
+    /*
     const last1 = num_list[num_list.length - 1];
     const last2 = num_list[num_list.length - 2];
     
     num_list.push(last1 > last2 ? last1 - last2 : last1 * 2)
     
     return num_list;
+    */
+    
+    const [a, b] = [...num_list].reverse();
+    
+    return [...num_list, a > b ? a-b : a*2];
 }
